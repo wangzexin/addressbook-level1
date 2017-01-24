092a34cc23829908d28569479528746ad37d8532
wangzexin <wang.zexin@dhs.sg>
2017-01-24 09:40:14 +0800

    Add default case statement to exit program to the method processProgramArgs()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -262,14 +262,12 @@ public class AddressBook {
         if (args.length >= 2) {
             showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
             exitProgram();
-        }
-
-        if (args.length == 1) {
+        } else if (args.length == 1) {
             setupGivenFileForStorage(args[0]);
-        }
-
-        if(args.length == 0) {
+        } else if(args.length == 0) {
             setupDefaultFileForStorage();
+        } else {
+        	exitProgram();
         }
     }
 
